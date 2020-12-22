92285e7679b9018788fc27beb23e5d2af1b401e0
julianmacagno <julian.macagno55@gmail.com>
2020-12-21 21:48:48 -0300

    Added cross-origin configuration

--- src/main/java/com/atm/JwtDemoApplication.java
+++ src/main/java/com/atm/JwtDemoApplication.java
@@ -1,15 +1,22 @@
 package com.atm;
 
-import com.atm.security.JWTAuthorizationFilter;
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.atm.security.JWTAuthorizationFilter;
 
 @SpringBootApplication
 public class JwtDemoApplication extends SpringBootServletInitializer {
@@ -17,21 +24,32 @@ public class JwtDemoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(JwtDemoApplication.class, args);
 	}
-	
+
 	@EnableWebSecurity
 	@Configuration
 	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
-				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-				.authorizeRequests()
-				.anyRequest().permitAll();
-//				.antMatchers(HttpMethod.POST, "/login").permitAll()
-//				.anyRequest().authenticated();
+			http.csrf().disable().cors().configurationSource(corsConfigurationSource()).and()
+					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests().antMatchers(HttpMethod.POST, "/login").permitAll().anyRequest()
+					.authenticated();
 		}
 	}
 
-}
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedOrigin("*");
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 
+}
